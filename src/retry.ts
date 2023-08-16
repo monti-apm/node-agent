81815cd3c05a43231d58dde4593aed31a7972d43
Leonardo Venturini <leovenbag@gmail.com>
2023-08-16 09:15:31 -0400

    add retry

---
+++ src/retry.ts
@@ -0,0 +1,72 @@
+import { randomBytes } from 'crypto';
+
+interface RetryOptions {
+  baseTimeout?: number;
+  exponent?: number;
+  maxTimeout?: number;
+  minTimeout?: number;
+  minCount?: number;
+  fuzz?: number;
+}
+
+export class Retry {
+  private baseTimeout: number;
+  private exponent: number;
+  private maxTimeout: number;
+  private minTimeout: number;
+  private minCount: number;
+  private fuzz: number;
+  private retryTimer: NodeJS.Timeout | null;
+
+  constructor({
+                baseTimeout = 1000,
+                exponent = 2.2,
+                maxTimeout = 5 * 60000,
+                minTimeout = 10,
+                minCount = 2,
+                fuzz = 0.5
+              }: RetryOptions = {}) {
+    this.baseTimeout = baseTimeout;
+    this.exponent = exponent;
+    this.maxTimeout = maxTimeout;
+    this.minTimeout = minTimeout;
+    this.minCount = minCount;
+    this.fuzz = fuzz;
+    this.retryTimer = null;
+  }
+
+  public clear(): void {
+    if (this.retryTimer) {
+      clearTimeout(this.retryTimer);
+    }
+    this.retryTimer = null;
+  }
+
+  private _timeout(count: number): number {
+    if (count < this.minCount) {
+      return this.minTimeout;
+    }
+
+    let timeout = Math.min(
+      this.maxTimeout,
+      this.baseTimeout * Math.pow(this.exponent, count)
+    );
+
+    timeout *= this.randomFraction() * this.fuzz + (1 - this.fuzz / 2);
+    return Math.ceil(timeout);
+  }
+
+  private randomFraction(): number {
+    return parseInt(randomBytes(8).toString('hex'), 16) / 0xffffffffffffffff;
+  }
+
+  public retryLater(count: number, fn: () => void): number {
+    const timeout = this._timeout(count);
+    if (this.retryTimer) {
+      clearTimeout(this.retryTimer);
+    }
+
+    this.retryTimer = setTimeout(fn, timeout);
+    return timeout;
+  }
+}
