d1e2d2ccfbe05efe699b985083fda620e9356253
Leonardo Venturini <leovenbag@gmail.com>
2023-08-28 08:54:26 -0400

    add config package

--- src/monti-apm-agent.ts
+++ src/monti-apm-agent.ts
@@ -1,23 +1,49 @@
 import { DocSzCache } from "@/doc-sz-cache";
+import { Monti } from "monti-apm-core";
+import os from "os";
+import { version } from '../package.json';
+import config from 'config'
 
 export class MontiApmAgent {
-  static _instance: MontiApmAgent;
+  static instance: MontiApmAgent;
 
-  static get instance() {
-    if (!MontiApmAgent._instance) {
-      MontiApmAgent._instance = new MontiApmAgent();
-    }
-    return MontiApmAgent._instance;
-  }
-
+  appSecret: string;
+  appId: string;
+  core: Monti;
 
   docSzCache = new DocSzCache(100000, 10);
 
-  constructor() {
-    console.log('MontiApmAgent constructor');
+  constructor(
+    appId: string,
+    appSecret: string,
+    endpoint: string = config.get('Monti.endpoint'),
+    hostname = config.get('Monti.hostname') || os.hostname(),
+  ) {
+    this.appId = appId;
+    this.appSecret = appSecret;
+
+    this.core = new Monti({
+      appId,
+      appSecret,
+      endpoint,
+      hostname,
+      agentVersion: `node-agent@${version}`
+    });
   }
 
-  public connect(appId: string, secret: string) {
-
+  static connect(
+    appId: string,
+    appSecret: string,
+    endpoint?: string,
+    hostname?: string,
+  ) {
+    MontiApmAgent.instance = new MontiApmAgent(appId, appSecret, endpoint, hostname);
   }
+}
+
+if (config.get('Monti.appId') && config.get('Monti.appSecret')) {
+  MontiApmAgent.connect(
+    config.get('Monti.appId'),
+    config.get('Monti.appSecret'),
+  );
 }
